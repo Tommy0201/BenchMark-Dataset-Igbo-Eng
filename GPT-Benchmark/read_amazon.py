ea89370526fe40c25f98601f070e91a4df7f1cab
Tommy0201 <ng.phucminh04@gmail.com>
2025-01-01 17:10:05 +0700

    renaming mturk files

--- GPT-Benchmark/read_amazon.py
+++ GPT-Benchmark/read_amazon.py
@@ -1,41 +1,54 @@
 import pandas as pd
 
 
-df = pd.read_csv("GPT-Benchmark/igbo_amazon.csv")
+def preprocessing_find_shortest(file_name):
+    df = pd.read_csv(file_name)
 
 
-unique_values = df["Reject"].unique()
+    unique_values = df["Reject"].unique()
 
-# Print the unique values
-print("Unique values in AssignmentStatus", ":", unique_values)
+    # Print the unique values
+    print("Unique values in AssignmentStatus", ":", unique_values)
 
-# Filter out rows where AssignmentStatus is "Rejected"
-filtered_df = df[df['AssignmentStatus'] != "Rejected"]
+    # Filter out rows where AssignmentStatus is "Rejected"
+    filtered_df = df[df['AssignmentStatus'] != "Rejected"]
 
-# Select specific columns
-selected_columns = filtered_df[['WorkTimeInSeconds', 'WorkerId', 'AssignmentStatus', 
-                                'Input.igbo_sent', 'Answer.translated', 'LifetimeApprovalRate']]
+    # Select specific columns
+    selected_columns = filtered_df[['WorkTimeInSeconds', 'WorkerId', 'AssignmentStatus', 
+                                    'Input.pid_sent', 'Answer.translated', 'LifetimeApprovalRate']]
 
-# Rename columns
-selected_columns = selected_columns.rename(columns={
-    'Input.pid_sent': 'igbo_input',
-    'Answer.translated': 'eng_output',
-})
+    # Rename columns
+    selected_columns = selected_columns.rename(columns={
+        'Input.pid_sent': 'pid_input',
+        'Answer.translated': 'eng_output',
+    })
 
-selected_columns['WorkTimeInMinutes'] = selected_columns['WorkTimeInSeconds'] / 60
+    selected_columns['WorkTimeInMinutes'] = selected_columns['WorkTimeInSeconds'] / 60
 
-# Calculate and print the number of unique WorkerId
-num_unique_workers = selected_columns['WorkerId'].nunique()
-print("Number of unique Worker IDs:", num_unique_workers)
+    # Calculate and print the number of unique WorkerId
+    num_unique_workers = selected_columns['WorkerId'].nunique()
+    print("Number of unique Worker IDs:", num_unique_workers)
 
-num_col = selected_columns.shape[0]
+    num_col = selected_columns.shape[0]
 
-print("Columns: ", num_col)
+    print("Columns: ", num_col)
 
-selected_columns.to_csv("GPT-Benchmark/sell1.csv",index=False)
+    selected_columns.to_csv("GPT-Benchmark/pid1.csv",index=False)
 
-shortest_times = selected_columns.nsmallest(20, 'WorkTimeInSeconds')
+    shortest_times = selected_columns.nsmallest(20, 'WorkTimeInSeconds')
 
-output_df = shortest_times[["WorkerId","WorkTimeInSeconds","LifetimeApprovalRate",'igbo_input','eng_output']]
+    output_df = shortest_times[["WorkerId","WorkTimeInSeconds","LifetimeApprovalRate",'pid_input','eng_output']]
 
-output_df.to_csv("GPT-Benchmark/shortest2.csv", index=False)
+    output_df.to_csv("GPT-Benchmark/pidgin_shortest2.csv", index=False)
+    
+if __name__ == "__main__":
+    # preprocessing_find_shortest("GPT-Benchmark/Pidgin_batch_results.csv")
+    df = pd.read_csv("GPT-Benchmark/Pidgin_batch_results.csv")
+    filtered_df = df[df['AssignmentStatus'] != "Rejected"]
+    selected_df = filtered_df[['Input.pid_sent', 'Answer.translated']]
+    # Rename columns
+    selected_df  = selected_df.rename(columns={
+        'Input.pid_sent': 'igbo_source',
+        'Answer.translated': 'eng_translation',
+    })
+    selected_df.to_csv("GPT-BenchMark/AmazonMTurk/Pidgin_Trans.csv", index=False)
